f64952c5bef2fbb9d7ce57065d541efcdf1b44e2
svloc <svloc39@gmail.com>
2022-07-05 12:51:28 +0530

    Series Ap

---
+++ Math/SeriesAp.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter A: ";
+    cin>>a;
+    int b;
+    cout<<"Enter B: ";
+    cin>>b;
+    int n;
+    cout<<"Enter N: ";
+    cin>>n;
+    cout<<"Result: "<<a+(n-1)*(b-a);
+    return 0;
+}
