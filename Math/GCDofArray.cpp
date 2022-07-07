403c926606c3e96f42e378bdfb49100f9d0221f7
svloc <svloc39@gmail.com>
2022-07-07 09:33:38 +0530

    gcd of array

---
+++ Math/GCDofArray.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int gcd_of_num(int a,int b){
+    int rem;
+    while(a%b!=0){
+        rem=a%b;
+        a=b;
+        b=rem;
+    }
+    return b;
+}
+int GCDofArray(int arr[],int n){
+  int x=0;
+  for(int i=0;i<n;i++){
+    x=gcd_of_num(x,arr[i]);
+  }
+  return x;
+}
+int main(){
+    int n;
+    cout<<"Enter N: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter N element: ";
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+    cout<<"Result: "<<GCDofArray(arr,n);
+    return 0;
+}
