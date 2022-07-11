ca19e3fddb9cbd3796770ce06f18c5bd110ae40f
svloc <svloc39@gmail.com>
2022-07-12 00:30:13 +0530

    updated fibonacci

--- Math/fibonacciNumber.cpp
+++ Math/fibonacciNumber.cpp
@@ -2,18 +2,22 @@
 using namespace std;
 int FibonacciNumber(int n)
 {
-    if (n == 0)
-        return -1;
+    if (n <= 0)
+        return 0;
     else if (n == 1)
         return 1;
     else
         return FibonacciNumber(n - 1) + FibonacciNumber(n - 2);
+}
+void AllFibonacciNum(int n){
+  for(int i=0;i<n;i++)
+  cout<<FibonacciNumber(i)<<" ";
 }
 int main()
 {
     int n;
     cout << "Enter N: ";
     cin >> n;
-    cout << "Result: " << FibonacciNumber(n);
+    AllFibonacciNum(n);
     return 0;
 }
