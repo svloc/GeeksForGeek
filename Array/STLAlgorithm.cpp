243b29f4ad248d399fd8cac236c650f7c74f5db0
svloc <svloc39@gmail.com>
2022-07-26 01:01:39 +0530

    stl

--- Array/STLAlgorithm.cpp
+++ Array/STLAlgorithm.cpp
@@ -13,19 +13,30 @@ int main(){
     cout<<"Enter K: ";
     cin>>k;
     if(binary_search(arr,arr+n,k))
-    cout<<k<<" Found--"<<endl;
+    cout<<k<<" Found--"<<distance(arr,k)<<endl;
     else
     cout<<"Not Found..";
 
-    reverse(arr,arr+n);
-    cout<<"Reverse Array: ";
-    for(int i=0;i<n;i++)
-    cout<<arr[i]<<" ";
-    cout<<endl;
+    // reverse(arr,arr+n);
+    // cout<<"Reverse Array: ";
+    // for(int i=0;i<n;i++)
+    // cout<<arr[i]<<" ";
+    // cout<<endl;
 
-    cout<<"Max: "<<*max_element(arr,arr+n);
-    cout<<"\nMin: "<<*min_element(arr,arr+n);
-    cout<<"\nSum: "<<accumulate(arr,arr+n,0);
-
+    // cout<<"Max: "<<*max_element(arr,arr+n);
+    // cout<<"\nMin: "<<*min_element(arr,arr+n);
+    // cout<<"\nSum: "<<accumulate(arr,arr+n,0);
+    // cout<<"\nEnter X: ";
+    // int x;
+    // cin>>x;
+    // cout<<"Count: "<<count(arr,arr+n,x);
+    // int y;
+    // cout<<"Enter Y";
+    // cin>>y;
+    //cout<<"\nFind: "<<find(arr,arr+n,y);
+    // arr.erase();
+    // int m=sizeof(arr)/sizeof(arr[0]);
+    // for(int i=0;i<m;i++)
+    // cout<<arr[i]<<" ";
     return 0;
 }
--- Array/tempCodeRunnerFile.cpp
+++ Array/tempCodeRunnerFile.cpp
@@ -1,1 +1,4 @@
-unordered_map
+arr.erase();
+    // int m=sizeof(arr)/sizeof(arr[0]);
+    // for(int i=0;i<m;i++)
+    // cout<<arr[i]<<" ";
