68300327665de6a0a047268369d37045a7c1e63b
svloc <svloc39@gmail.com>
2022-11-08 09:46:26 +0530

    Queue

--- Queue/q2.cpp
+++ Queue/q2.cpp
@@ -1,13 +1,22 @@
 #include<bits/stdc++.h>
-using namesapce std;
+using namespace std;
 
+void print_queue(queue<int>q){
+  queue<int>temp=q;
+  while(!temp.empty()){
+    cout<<temp.front()<<" ";
+    temp.pop();
+  }
+  cout<<endl;
+}
 int main(){
     queue<int>q;
     for(int i=0;i<=20;i+=2)
     q.push(i);
 
-
-    
+    print_queue(q);
+    q.pop();
+    print_queue(q);
     return 0;
 
 }
